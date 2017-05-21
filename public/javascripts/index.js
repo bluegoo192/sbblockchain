f77f9ff1d5e648ea45e0c35c13c77ebadaf9475b
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-05-21 11:51:15 -0700

    added changing buzzwords to slogan

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,14 @@
 var app = new Vue({
   el: '#app',
   data: {
+    buzzwordCounter: 0,
+    buzzwords: [
+      "distributed",
+      "secure",
+      "reliable",
+      "transparent",
+      "decentralized"
+    ],
     show: {
       mobilenav: false
     }
@@ -9,5 +17,18 @@ var app = new Vue({
     test: function () {
       console.log('hi');
     }
+  },
+  computed: {
+    currentBuzzword: function () {
+      if (this.buzzwordCounter >= this.buzzwords.length) {
+        this.buzzwordCounter = 0;
+      }
+      return this.buzzwords[this.buzzwordCounter]
+    }
+  },
+  mounted: function () {
+    setInterval(function () {
+      this.buzzwordCounter += 1
+    }.bind(this), 2000)
   }
 })
